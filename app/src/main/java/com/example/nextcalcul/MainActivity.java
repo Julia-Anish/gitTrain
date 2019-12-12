eeb4417b5a806ab59d46f2a5d72aea3dcfb7d3c5
Julia_Anish <julia.anishchenko17@gmail.com>
2019-12-12 13:28:28 +0300

    Add menu with 2 items

--- app/src/main/java/com/example/nextcalcul/MainActivity.java
+++ app/src/main/java/com/example/nextcalcul/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.nextcalcul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
 
     EditText num1;
     EditText num2;
@@ -74,8 +78,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         res.setText(number1 + "" + oper + "" + number2 +  "=" + result);
 
-
-
-
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        menu.add (0, MENU_RESET_ID, 0, "Reset");
+        menu.add (0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()){
+            case MENU_RESET_ID:
+                num1.setText("");
+                num2.setText("");
+                res.setText("");
+                break;
+            case MENU_QUIT_ID:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
